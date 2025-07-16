1bcd5df6066906be8257f5b50762db913a0c4433
Sai <sailakshmy94@gmail.com>
2025-07-16 08:42:29 +0400

    Created a custom pipe to sort an arry

---
+++ src/app/sort.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "sort",
+  standalone: true,
+})
+export class SortPipe implements PipeTransform {
+  transform(value: string[] | number[], dir: "asc" | "desc" = "asc") {
+    const sorted = [...value];
+    sorted.sort((a, b) => {
+      if (dir === "asc") {
+        return a > b ? 1 : -1;
+      } else return b > a ? 1 : -1;
+    });
+    return sorted;
+  }
+}
