cf1b50e2b33422fa94856c2e9041fbea7abc8309
Sai <sailakshmy94@gmail.com>
2025-07-16 08:01:15 +0400

    Created a custom pipe

---
+++ src/app/temperature.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "temp",
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform {
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return `${value}-Transformed`;
+  }
+}
